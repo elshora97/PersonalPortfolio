23182b0bb35e1d92455d6dede6e55b135832f82f
elshora97 <A.elshora@dataserve.com.sa>
2024-01-02 01:28:49 +0200

    Added Projet V1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+  $("#menu").click(() => {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
+
+  $(window).on("scroll load", () => {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
+
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
+
+  // smooth scrolling
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
